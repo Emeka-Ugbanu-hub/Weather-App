dec0300bff3babb26ecca24e8cd3b107d432a45a
Emeka-Ugbanu-hub <jemeka865@gmail.com>
2020-11-14 15:52:27 +0100

    learning react

--- src/index.js
+++ src/index.js
@@ -1,78 +1,25 @@
-import React,{useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Button,Typography,TextField,Switch,Checkbox,Paper} from '@material-ui/core';
 import * as serviceWorker from './serviceWorker';
 
-const api = {
-  key:"278b8f47d0c87b7441667cb98d6b6ae9",
-  url:`https://api.openweathermap.org/data/2.5/`
-}
-
-function  App(){
-  const [name,setName] = useState("nigeria");
-  const [weather,setWeather] = useState("app");
-   const [temprature,setTemp] = useState("15°c");
-   const [degree,setDegree] = useState(" Cold");
-  const date = (d) =>{
- const arrofday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",];
- const arrofyear = ["January","Febuary","March","April","May","June","July","August","September",
- "October","November","December"]
- const day = arrofday[d.getDay()];
- const num = d.getDate();
- const month = arrofyear[d.getMonth()];
- const year = d.getFullYear();
- return (
-   `
-   ${day}
-   ${num}
-  ${month}
-  ${year}
-  `
- );
-  }
-  const handlePress = (event) =>{
-if(event.key === "Enter"){
-  const url = `${api.url}weather?q=${name}&appid=${api.key}&units=metric`;
-  fetch(url)
-  .then(response => response.json())
-  .then(data => {
-var { main} = data;
-if( main.temp > 24){
-setWeather("app warm")
-  setDegree("Warm")  
-}
-else{
-  setWeather("app") 
-  setDegree("Cold") 
-}
-setTemp(`${Math.floor(main.temp)}°c`)
-  })
-  .catch((e)=>{
-    alert("Please Turn On Your Internet Connectionb OR Search For A Valid City Or Country")
-  })
-}
-  }
-  const handleChange = (e) => {
-   const val = e.target.value;
-   setName(val)
-    }
-  return (
-    <div className={weather}>
-      <main>
-  <input className="input" onChange={handleChange} onKeyPress={handlePress} type="text" placeholder="Search..."/>
-  <div className="location__box">
-  <div className="location">{name}</div>
-  <div className="date">{date(new Date())}</div>
-  </div>
-  <div className="weather__box">
-<span className="temp">{temprature}</span>
-<div className="weather">
-  {degree}
-</div>
-  </div>
-  </main>
-  </div>
-  )
+function  App(){ 
+ return(
+   <>
+   <h1>HELLO WORLD</h1>
+   <Typography variant="h1">Learning material ui</Typography>
+  
+   <TextField color="secondary" id="time" variant="outlined" type="text"  />
+    <TextField color="primary" id="time" variant="outlined" type="email" placeholder="test@gmail.com"  />
+    <Checkbox color="primary"></Checkbox>
+    <Switch></Switch>
+    <Paper color="primary" variant="outlined" elevation={0} />
+<Paper />
+   <Button  variant="contained" color="primary">Submit</Button>
+   </>
+ )
+ 
 }
 
 ReactDOM.render(
